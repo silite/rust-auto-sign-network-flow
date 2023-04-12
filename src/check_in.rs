4ed9866fe46c22550af9f77af3d6e2e31af8f3ec
silite <lemnsilite@gmail.com>
2023-04-12 10:37:51 +0800

    fix: decode

--- src/check_in.rs
+++ src/check_in.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 
+use mysql::serde_json::from_str;
 use serde::Deserialize;
 
 use crate::{email::send_email, mysql_conn::insert_log, CLIENT};
@@ -19,7 +20,16 @@ pub async fn check_in() -> Result<(), Box<dyn Error>> {
         .text()
         .await?;
 
+    let res: String = from_str(&res)?;
     send_email("签到结果", res.as_str())?;
     insert_log(res.as_str())?;
     Ok(())
+}
+
+#[test]
+fn feature() {
+    let ch = '汉' as i32;
+    let ch_unicode = format!("{:X}", ch);
+    println!("{}", ch);
+    println!("{}", ch_unicode);
 }
